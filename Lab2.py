3c8e4de8053a1d486079c3b27277e29f79f1aa59
Nechelovek <ivan.batura.programmer@gmail.com>
2020-06-03 11:05:47 +0300

    Last commit

--- Lab2.py
+++ Lab2.py
@@ -85,193 +85,193 @@ def ode(y, t) :
 drawPhasePortrait(-7.5, 7.5, -7, 6,  1, 1, -5, 5, -7, 7, ts = 0.5, nt = 301)
 #drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
 
-# #System 4
-# def ode(y, t) :
-#     x, y = y
-#     dydt = [-0.1 * variant * x * x + 2 * y , -3 * x -y]
-#     return dydt
-#
-# drawPhasePortrait(-5 , 4, -5, 5, 1, 1, -5, 5, -5, 5, ts = 0.6, nt = 201)
-#
-# def ode(y, t) :
-#     x, y = y
-#     dydt = [2 * y , -3 * x - y]
-#     return dydt
-#
-# drawPhasePortrait(-5 , 5, -5, 5, 1, 1, -5, 5, -5, 5, ts = 1, nt = 201)
-#
-#
-# #System 5
-# def ode(y, t) :
-#     x, y = y
-#     dydt = [0.1 * x - 4 * y, 4 * x - 0.2 * variant * x * x + 0.1 * y]
-#     return dydt
-#
-# drawPhasePortrait(-12, 12, -10, 10, 1, 1, -10, 10, -10, 10, ts = 0.2, nt = 201)
-#
-# def ode(y, t) :
-#     x, y = y
-#     dydt = [0.1 * x - 4 * y, 4 * x + 0.1 * y]
-#     return dydt
-#
-# drawPhasePortrait(-12, 12, -10, 10, 1, 1, -10, 10, -10, 10, ts = 0.2, nt = 201)
-# #drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
-#
-#
-# #System 6
-# #non lineary system
-# def ode(Y, t) :
-#     x, y = Y
-#     dydt = [x - 0.1 * variant * x * x - 4 * y + 0.3 * variant * y * y, 2*x + 0.2*variant*x*x - y - 0.3*variant * y * y * y]
-#     return dydt
-#
-# drawPhasePortrait(-10, 6, -4.5, 4.5, 1, 1, -8, 5, -5, 5, ts = 0.35, nt = 301)
-# #drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
-#
-# def ode(Y, t) :
-#     x, y = Y
-#     dydt = [x - 4 * y, 2*x - y]
-#     return dydt
-#
-# drawPhasePortrait(-12, 8, -6, 4.5, 1, 1, -8, 5, -5, 5, ts = 0.35, nt = 301)
-# #drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
-#
-#
-# # #Task 2
-# def dead_zone_scalar(x, wight = 0.4):
-#     if np.abs(x) < wight:
-#         return 0
-#     elif x > 0:
-#         return x - wight
-#     else:
-#         return x + wight
-#
-# dead_zone = np.vectorize(dead_zone_scalar, otypes = [np.float], excluded = ['width'])
-#
-#
-# def ode(Y, t):
-#     x, y = Y
-#     dydt = [y, np.sign(dead_zone(1 - x)) - y]
-#     return dydt
-#
-# drawPhasePortrait(-1, 3, -6, 4.5, 1, 1, -3, 4, -5, 5, ts = 4, nt = 301)
-#
-#
-# #Quest 3
-# m = 0.2 * variant
-# l = 5 / variant
-# g = 9.81
-# b = 0.1 + 0.015 * variant
-#
-# def ode(y, t) :
-#     theta, omega = y
-#     dydt = [omega, -0.2 * 9.81 * 5 * np.sin(theta)]
-#     return dydt
-#
-# drawPhasePortrait(-10, 10, -8, 8, 1, 1, -5, 5, -4, 4, ts = 5, nt = 301)
-#
-# def ode(y, t) :
-#     theta, omega = y
-#     dydt = [omega, (-0.115 * omega) - (0.2 * 9.81 * 5 * np.sin(theta))]
-#     return dydt
-#
-# drawPhasePortrait(-10, 10, -8, 8, 1, 1, -8, 8, -4, 4, ts = 5, nt = 1000)
-#
-# #Quest 4
-# m1 = variant
-# m2 = variant / 2
-# m3 = 2 * variant
-#
-# def odeVanDerPaul(Y, t, mu):
-#     x, y = Y
-#     dydt = [y, mu * (1 - (x ** 2)) * y - x]
-#     return dydt
-#
-# def calcODEVanDerPaul(args, y0, dy0, ts=10, nt=101):
-#     y0 = [y0, dy0]
-#     t = np.linspace(0, ts, nt)
-#     sol = odeint(odeVanDerPaul, y0, t, args)
-#     return sol
-#
-# def vanDerPaul(args, deltaX=1, deltaDX=1, startX=0, stopX=5, startDX=0, stopDX=5, ts=10, nt=101):
-#     for y0 in range(startX, stopX, deltaX):
-#         for dy0 in range(startDX, stopDX, deltaDX):
-#             sol = calcODEVanDerPaul(args, y0, dy0, ts, nt)
-#             plt.plot(sol[:, 0], sol[:, 1], 'b')
-#     plt.xlabel('x')
-#     plt.ylabel('dx/dt')
-#     plt.xlim()
-#     plt.ylim()
-#     plt.grid()
-#     plt.show()
-#
-# args = (m1, )
-# vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
-#
-# args = (m1, )
-# vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
-#
-# args = (m3, )
-# vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
+#System 4
+def ode(y, t) :
+    x, y = y
+    dydt = [-0.1 * variant * x * x + 2 * y , -3 * x -y]
+    return dydt
 
-# # #Quest 5
-# def ode(y, t, sigma, r, b):
-#     x, y, z = y
-#     dxdt = sigma * (y - x)
-#     dydt = x * (r - z) - y
-#     dzdt = x * y - b * z
-#     return [dxdt, dydt, dzdt]
-#
-#
-# def calcODE(args, x, y, z, ts=10, nt=101):
-#     y0 = [x, y, z]
-#     t = np.linspace(0, ts, nt)
-#     sol = odeint(ode, y0, t, args)
-#     return sol
-#
-#
-# def drawPhasePortrait3D(args,
-#                         deltaX=1, deltaY=1, deltaZ=1,
-#                         startX=0, stopX=5,
-#                         startY=0, stopY=5,
-#                         startZ=0, stopZ=5,
-#                         ts=10, nt=101):
-#     fig = plt.figure()
-#     ax = fig.add_subplot(2, 2, 1, projection='3d')
-#     ax.set_title("3D")
-#     plt.subplot(2, 2, 2)
-#     plt.title("X-Y")
-#     plt.grid()
-#     plt.subplot(2, 2, 3)
-#     plt.title("X-Z")
-#     plt.grid()
-#     plt.subplot(2, 2, 4)
-#     plt.title("Y-Z")
-#     plt.grid()
-#
-#     for x in range(startX, stopX, deltaX):
-#         for y in range(startY, stopY, deltaY):
-#             for z in range(startZ, stopZ, deltaZ):
-#                 sol = calcODE(args, x, y, z, ts, nt)
-#
-#                 ax.plot(sol[:, 0], sol[:, 1], sol[:, 2])
-#                 plt.subplot(2, 2, 2)
-#                 plt.plot(sol[:, 0], sol[:, 1])
-#                 plt.subplot(2, 2, 3)
-#                 plt.plot(sol[:, 0], sol[:, 2])
-#                 plt.subplot(2, 2, 4)
-#                 plt.plot(sol[:, 1], sol[:, 2])
-#
-#     plt.show()
-#
-#
-# sigma = 60
-# r = 28
-# b = 8 / 3
-# args = (sigma, r, b)
-# drawPhasePortrait3D(args,
-#                     deltaX=8, deltaY=8, deltaZ=8,
-#                     startX=-10, stopX=10,
-#                     startY=-10, stopY=10,
-#                     startZ=-10, stopZ=10,
-#                     ts=0.5, nt=500)
+drawPhasePortrait(-5 , 4, -5, 5, 1, 1, -5, 5, -5, 5, ts = 0.6, nt = 201)
+
+def ode(y, t) :
+    x, y = y
+    dydt = [2 * y , -3 * x - y]
+    return dydt
+
+drawPhasePortrait(-5 , 5, -5, 5, 1, 1, -5, 5, -5, 5, ts = 1, nt = 201)
+
+
+#System 5
+def ode(y, t) :
+    x, y = y
+    dydt = [0.1 * x - 4 * y, 4 * x - 0.2 * variant * x * x + 0.1 * y]
+    return dydt
+
+drawPhasePortrait(-12, 12, -10, 10, 1, 1, -10, 10, -10, 10, ts = 0.2, nt = 201)
+
+def ode(y, t) :
+    x, y = y
+    dydt = [0.1 * x - 4 * y, 4 * x + 0.1 * y]
+    return dydt
+
+drawPhasePortrait(-12, 12, -10, 10, 1, 1, -10, 10, -10, 10, ts = 0.2, nt = 201)
+#drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
+
+
+#System 6
+#non lineary system
+def ode(Y, t) :
+    x, y = Y
+    dydt = [x - 0.1 * variant * x * x - 4 * y + 0.3 * variant * y * y, 2*x + 0.2*variant*x*x - y - 0.3*variant * y * y * y]
+    return dydt
+
+drawPhasePortrait(-10, 6, -4.5, 4.5, 1, 1, -8, 5, -5, 5, ts = 0.35, nt = 301)
+#drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
+
+def ode(Y, t) :
+    x, y = Y
+    dydt = [x - 4 * y, 2*x - y]
+    return dydt
+
+drawPhasePortrait(-12, 8, -6, 4.5, 1, 1, -8, 5, -5, 5, ts = 0.35, nt = 301)
+#drawPhasePortrait(args, 1, 1, -5, 5, -4, 4, ts = 0.5, nt = 301)
+
+
+# #Task 2
+def dead_zone_scalar(x, wight = 0.4):
+    if np.abs(x) < wight:
+        return 0
+    elif x > 0:
+        return x - wight
+    else:
+        return x + wight
+
+dead_zone = np.vectorize(dead_zone_scalar, otypes = [np.float], excluded = ['width'])
+
+
+def ode(Y, t):
+    x, y = Y
+    dydt = [y, np.sign(dead_zone(1 - x)) - y]
+    return dydt
+
+drawPhasePortrait(-1, 3, -6, 4.5, 1, 1, -3, 4, -5, 5, ts = 4, nt = 301)
+
+
+#Quest 3
+m = 0.2 * variant
+l = 5 / variant
+g = 9.81
+b = 0.1 + 0.015 * variant
+
+def ode(y, t) :
+    theta, omega = y
+    dydt = [omega, -0.2 * 9.81 * 5 * np.sin(theta)]
+    return dydt
+
+drawPhasePortrait(-10, 10, -8, 8, 1, 1, -5, 5, -4, 4, ts = 5, nt = 301)
+
+def ode(y, t) :
+    theta, omega = y
+    dydt = [omega, (-0.115 * omega) - (0.2 * 9.81 * 5 * np.sin(theta))]
+    return dydt
+
+drawPhasePortrait(-10, 10, -8, 8, 1, 1, -8, 8, -4, 4, ts = 5, nt = 1000)
+
+#Quest 4
+m1 = variant
+m2 = variant / 2
+m3 = 2 * variant
+
+def odeVanDerPaul(Y, t, mu):
+    x, y = Y
+    dydt = [y, mu * (1 - (x ** 2)) * y - x]
+    return dydt
+
+def calcODEVanDerPaul(args, y0, dy0, ts=10, nt=101):
+    y0 = [y0, dy0]
+    t = np.linspace(0, ts, nt)
+    sol = odeint(odeVanDerPaul, y0, t, args)
+    return sol
+
+def vanDerPaul(args, deltaX=1, deltaDX=1, startX=0, stopX=5, startDX=0, stopDX=5, ts=10, nt=101):
+    for y0 in range(startX, stopX, deltaX):
+        for dy0 in range(startDX, stopDX, deltaDX):
+            sol = calcODEVanDerPaul(args, y0, dy0, ts, nt)
+            plt.plot(sol[:, 0], sol[:, 1], 'b')
+    plt.xlabel('x')
+    plt.ylabel('dx/dt')
+    plt.xlim()
+    plt.ylim()
+    plt.grid()
+    plt.show()
+
+args = (m1, )
+vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
+
+args = (m1, )
+vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
+
+args = (m3, )
+vanDerPaul(args, 1, 1, -4, 5, -5, 5, ts = 20, nt = 2001)
+
+# #Quest 5
+def ode(y, t, sigma, r, b):
+    x, y, z = y
+    dxdt = sigma * (y - x)
+    dydt = x * (r - z) - y
+    dzdt = x * y - b * z
+    return [dxdt, dydt, dzdt]
+
+
+def calcODE(args, x, y, z, ts=10, nt=101):
+    y0 = [x, y, z]
+    t = np.linspace(0, ts, nt)
+    sol = odeint(ode, y0, t, args)
+    return sol
+
+
+def drawPhasePortrait3D(args,
+                        deltaX=1, deltaY=1, deltaZ=1,
+                        startX=0, stopX=5,
+                        startY=0, stopY=5,
+                        startZ=0, stopZ=5,
+                        ts=10, nt=101):
+    fig = plt.figure()
+    ax = fig.add_subplot(2, 2, 1, projection='3d')
+    ax.set_title("3D")
+    plt.subplot(2, 2, 2)
+    plt.title("X-Y")
+    plt.grid()
+    plt.subplot(2, 2, 3)
+    plt.title("X-Z")
+    plt.grid()
+    plt.subplot(2, 2, 4)
+    plt.title("Y-Z")
+    plt.grid()
+
+    for x in range(startX, stopX, deltaX):
+        for y in range(startY, stopY, deltaY):
+            for z in range(startZ, stopZ, deltaZ):
+                sol = calcODE(args, x, y, z, ts, nt)
+
+                ax.plot(sol[:, 0], sol[:, 1], sol[:, 2])
+                plt.subplot(2, 2, 2)
+                plt.plot(sol[:, 0], sol[:, 1])
+                plt.subplot(2, 2, 3)
+                plt.plot(sol[:, 0], sol[:, 2])
+                plt.subplot(2, 2, 4)
+                plt.plot(sol[:, 1], sol[:, 2])
+
+    plt.show()
+
+
+sigma = 60
+r = 28
+b = 8 / 3
+args = (sigma, r, b)
+drawPhasePortrait3D(args,
+                    deltaX=8, deltaY=8, deltaZ=8,
+                    startX=-10, stopX=10,
+                    startY=-10, stopY=10,
+                    startZ=-10, stopZ=10,
+                    ts=0.5, nt=500)
---
+++ Lab3.py
@@ -0,0 +1,416 @@
+from scipy.integrate import odeint
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+variant = 1
+
+class real_object:
+    def __init__(self, variant):
+        self._var = variant
+        self._ctrl_fcn = real_object._default_control
+        self.lin_par_1 = variant % 10 * 0.2
+        self.lin_par_2 = ((32 - variant) % 9 + 0.1) * 2.5
+        self.nonlin_par_1 = variant % 15 * 0.35
+        self.nonlin_par_2 = variant % 12 * 0.45
+        self.nonlin_fcns = [self.deadZone, self.saturation, self.relay]
+        self.nonlin_names = ['deadZone', 'saturation', 'relay']
+        self.nonlin_type = variant % 3
+        self._params = [self.lin_par_1, self.lin_par_2, self.nonlin_par_1, self.nonlin_par_2]
+        print(self.lin_par_1, self.lin_par_2, self.nonlin_par_1, self.nonlin_par_2)
+
+    def deadZone(self, x, p1, p2):
+        if np.abs(x) < p1:
+            x = 0
+        elif x > 0:
+            x = x - p1
+        elif x < 0:
+            x = x + p2
+        return x
+
+    def saturation(self, x, p1, p2):
+        if x > p1:
+            x = p1
+        elif x < -p2:
+            x = -p2
+        return x
+
+    def relay(self, x, p1, p2):
+        if x > 0:
+            return p1
+        else:
+            return -p2
+
+    def _ode(self, x, t, k):
+        '''
+        Функция принимает на вход вектор переменных состояния и реализует по сути систему в форме Коши
+        x -- текущий вектор переменных состояния
+        t -- текущее время
+        k -- значения параметров
+        '''
+        y = x
+        u = self._get_u(x, t)
+        lin_par_1, lin_par_2, nonlin_par_1, nonlin_par_2 = k
+
+        dydt = (lin_par_1 * self.nonlin_fcns[self.nonlin_type](u, nonlin_par_1, nonlin_par_2) - x) / lin_par_2
+        return dydt
+
+    def _default_control(x, t):
+        """
+        Управление по умолчанию. Нулевой вход
+        """
+        return 0
+
+    def _get_u(self, x, t):
+        """
+        Получить значение управления при значениях переменных состояния x и времени t
+        """
+        return self._ctrl_fcn(x, t)
+
+    def set_u_fcn(self, new_u):
+        """
+        Установить новую функцию управления
+        формат функции: fcn(x, t)
+        """
+        self._ctrl_fcn = new_u
+
+    def calcODE(self, y0, ts=10, nt=1001):
+        """
+        Вспомогательная функция для получения решения систему ДУ, "Проведение эксперимента" с заданным воздействием
+        """
+        y0 = [y0, ]
+        t = np.linspace(0, ts, nt)
+        args = (self._params,)
+        sol = odeint(self._ode, y0, t, args)
+        return sol, t
+
+    def getODE(self):
+        """
+        Получить "идеальную" модель объекта без параметров
+        """
+        return self._ode
+
+    def get_nonlinear_element_type(self):
+        return self.nonlin_names[self.nonlin_type]
+
+obj = real_object(variant)
+y0 = 1
+sol, t = obj.calcODE(y0)
+plt.plot(t, sol)
+plt.grid()
+#plt.show()
+
+
+def exp(sig):
+    # Создаем экземпляр класса объекта
+    obj = real_object(variant)
+
+    # Задаем функцию управления
+    obj.set_u_fcn(sig)
+
+    # Задаем начальные условия
+    y0 = 1
+
+    # Проводим эксперимент
+    sol, t = obj.calcODE(y0)
+
+    # И строим управляющее воздействие. В случае, если функция управления -- скалярная, то ее стоит векторизовать.
+    u = sig(0, t)
+    plt.plot(t, u)
+    plt.plot(t, sol)
+    plt.grid()
+    #plt.show()
+
+def monoharm_e(x, t):
+    return 1*np.sin(t * 0.5 * np.pi)
+
+#exp(monoharm_e)
+
+def monoharm_u(x, t):
+    return 3*np.sin(t * 0.5 * np.pi)
+
+#exp(monoharm_u)
+
+def step_function(x, t):
+    if(t >= 0):
+        return 1
+    else:
+        return 0
+step_function = np.vectorize(step_function, otypes = [np.float])
+
+#exp(step_function)
+
+def impulse(x,t): #Aproksimatiya
+  if t < 0.01:
+    return 100
+  else:
+    return 0
+impulse = np.vectorize(impulse,  otypes = [np.float])
+
+#exp(impulse)
+
+def square(x, t):
+    return x * signal.square(t * 2 * np.pi)
+
+#exp(square)
+
+def whitenoise(x, t):
+  return np.random.normal(1,1)
+whitenoise = np.vectorize(whitenoise, otypes=[np.float])
+
+#exp(whitenoise)
+
+#Chapter 2
+from scipy import optimize
+from scipy import integrate, interpolate
+
+
+class parameter_estimator():
+    def __init__(self, experiments, f):
+        """
+        experiments -- список кортежей с данными экспериментов в формате [x_data, y_data] (вход, выход)
+        f -- функция, реализующая дифференциальное уравнение модели
+        """
+        self._experiments = experiments
+        self._f = f
+        # Предполагаем, что все переменные состояния наблюдаемые, однако в общем случае это не так
+        x_data, y_data = experiments[0]
+        self.n_observed = 1  # y_data.shape[1]
+
+    def my_ls_func(self, x, teta):
+        """
+        Определение функции, возвращающей значения решения ДУ в
+        процессе оценки параметров
+        x заданные (временные) точки, где известно решение
+        (экспериментальные данные)
+        teta -- массив с текущим значением оцениваемых параметров.
+        Первые self._y0_len элементов -- начальные условия,
+        остальные -- параметры ДУ
+        """
+        # Для передачи функуии используем ламбда-выражение с подставленными
+        # параметрами
+        # Вычислим значения дифференциального уравления в точках "x"
+        r = integrate.odeint(lambda y, t: self._f(y, t, teta[self._y0_len:]),
+                             teta[0:self._y0_len], x)
+        # Возвращаем только наблюдаемые переменные
+        return r[:, 0:self.n_observed]
+
+    def estimate_ode(self, y0, guess):
+        """
+        Произвести оценку параметров дифференциального уравнения с заданными
+        начальными значениями параметров:
+            y0 -- начальные условия ДУ
+            guess -- параметры ДУ
+        """
+        # Сохраняем число начальных условий
+        self._y0_len = len(y0)
+        # Создаем вектор оцениваемых параметров,
+        # включающий в себя начальные условия
+        est_values = np.concatenate((y0, guess))
+        c = self.estimate_param(est_values)
+        # В возвращаемом значении разделяем начальные условия и параметры
+        return c[self._y0_len:], c[0:self._y0_len]
+
+    def f_resid(self, p):
+        """
+        Функция для передачи в optimize.leastsq
+        При дальнейших вычислениях значения, возвращаемые этой функцией,
+        будут возведены в квадрат и просуммированы.
+
+        """
+        delta = []
+        # Получаем ошибку для всех экспериментов при заданных параметрах модели
+        for data in self._experiments:
+            x_data, y_data = data
+            d = y_data - self.my_ls_func(x_data, p)
+            d = d.flatten()
+            delta.append(d)
+        delta = np.array(delta)
+
+        return delta.flatten()  # Преобразуем в одномерный массив
+
+    def calcODE(self, args, y0, x0=0, xEnd=10, nt=1001):
+        """
+        Служебная функция для решения ДУ
+        """
+        t = np.linspace(x0, xEnd, nt)
+        sol = odeint(self._f, y0, t, args)
+        return sol, t
+
+    def estimate_param(self, guess):
+        """
+        Произвести оценку параметров ДУ
+            guess -- параметры ДУ
+        """
+        self._est_values = guess
+        # Решить оптимизационную задачу - решение в переменной c
+        res = optimize.least_squares(self.f_resid, self._est_values)
+        return res.x
+
+def ideal_model(signal_type):
+    guess = [0.6, 5.25, 1.05, 1.35]  # Начальные значения для параматров системы
+    y0 = [1, ]  # Стартовые начальные значения для системы ДУ
+
+    obj = real_object(variant)
+    obj.set_u_fcn(signal_type)
+    sol, t = obj.calcODE(y0[0])
+
+    estimator = parameter_estimator([[t, sol], ], obj.getODE())
+    est_par = estimator.estimate_ode(y0, guess)
+    print("Estimated parameter: {}".format(est_par[0]))
+    print("Estimated initial condition: {}".format(est_par[1]))
+
+     # Строим графики входа и выходов системы
+    y0 = est_par[1]
+    args = (est_par[0],)
+    sol_ideal = odeint(obj.getODE(), y0, t, args)
+
+    # Строим графики входа и выходов системы
+    plt.plot(t, sol_ideal)
+    plt.plot(t, sol)
+    plt.grid()
+    plt.show()
+
+
+# ideal_model(monoharm_e)
+# ideal_model(monoharm_u)
+# ideal_model(step_function)
+# ideal_model(impulse)
+# ideal_model(square)
+# ideal_model(whitenoise)
+
+def ode_lin(x, t, k):
+    """
+    Функция, рализующая систему ДУ маятника с трением
+    """
+    y = x
+    K = k[0]
+    T = k[1]
+    u = monoharm_u(0, t)
+    dydt = (K*u-y)/T
+    return dydt
+
+def lineary_model(signal):
+    guess = [0.2, 0.3]  # Начальные значения для параматров системы
+    y0 = [0,]  # Стартовые начальные значения для системы ДУ
+
+    obj = real_object(variant)
+    obj.set_u_fcn(signal)
+    sol, t = obj.calcODE(y0[0])
+
+    estimator = parameter_estimator([[t, sol],], ode_lin)
+    est_par = estimator.estimate_ode(y0, guess)
+    print("Estimated parameter: {}".format(est_par[0]))
+    print("Estimated initial condition: {}".format(est_par[1]))
+
+    y0 = est_par[1]
+    args = (est_par[0],)
+    sol_lin = odeint(ode_lin, y0, t, args)
+
+    plt.plot(t, sol_lin)
+    plt.plot(t, sol)
+    plt.grid()
+    plt.show()
+
+# lineary_model(monoharm_e)
+# lineary_model(monoharm_u)
+# lineary_model(step_function)
+# lineary_model(impulse)
+# lineary_model(square)
+# lineary_model(whitenoise)
+
+
+
+
+#non_lineary_model(monoharm_e)
+#non_lineary_model(monoharm_u)
+#non_lineary_model(step_function)
+#non_lineary_model(impulse)
+#non_lineary_model(square)
+#non_lineary_model(whitenoise)
+
+def series2dataset(data, seq_len):
+    """
+    Преобразование временной последовательнсти к формату датасета
+    Шаг дискретизации должен быть постоянным
+    """
+    dataset = []
+    for i in range(data.shape[0]-seq_len):
+        r = np.copy(data[i:i+seq_len])
+        dataset.append(r)
+    return np.array(dataset)
+
+from sklearn.preprocessing import MinMaxScaler
+# normalize features
+scaler = MinMaxScaler(feature_range=(-1, 1))
+
+values = monoharm_u(0, t)
+model_order = 5
+# Этой командой получается массив обучающих последовательностей с одного
+# эксперимента, для работы с несколькими экспериментами, можно получить
+# последовательно массивы отдельно по каждому эксперименту, а затем объединить
+# массивы
+x_values = series2dataset(values, model_order)
+x_values = np.expand_dims(x_values,2)
+print(x_values.shape)
+
+# Разделим на тестовую и обучающие выборки
+# В случае использования нескольких экспериментов (>5), в качестве тестового
+# лучше взять один из экспериментов целиком
+n_train_samples = int(x_values.shape[0] * 0.7)
+
+train_X = x_values[:n_train_samples, :]
+test_X = x_values[n_train_samples:, :]
+
+y_values = scaler.fit_transform(sol)
+y_values = y_values[model_order:]
+train_y = y_values[:n_train_samples]
+test_y = y_values[n_train_samples:]
+
+print("Shape of train is {}, {}, shape of test is {}, {}".format(train_X.shape,
+                                                                 train_y.shape,
+                                                                 test_X.shape,
+                                                                 test_y.shape))
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# fit network
+history = model.fit(train_X,
+                    train_y,
+                    epochs=100,
+                    batch_size=72,
+                    validation_data=(test_X, test_y),
+                    verbose=1,
+                    shuffle=False)
+# plot history
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.show()
+
+#
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.metrics import mean_squared_error
+# from math import sqrt
+#
+# #делаем предсказание
+# yhat = model.predict(test_X)
+#
+# # обратное масштабирование для прогноза
+# inv_yhat = yhat #scaler.inverse_transform(yhat)
+# #inv_yhat = inv_yhat[:,0]
+# # обратное масштабирование для фактического
+# inv_y = test_y #scaler.inverse_transform(test_y)
+# #inv_y = inv_y[:,0]
+# # calculate RMSE
+# rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+# print('Test RMSE: %.3f' % rmse)
+
